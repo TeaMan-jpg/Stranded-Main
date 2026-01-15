c001f8d4728f76b6962fe04249206c9315ec7645
TeaMan-jpg <tomtoby125@gmail.com>
2026-01-15 17:58:44 +0000

    settings changes

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -35,36 +35,35 @@ public class SettingsMenu : MonoBehaviour
     }
     void Start()
     {
-        //SetupResolutionDropdown();
+        SetupResolutionDropdown();
         LoadSettings();
-        //SetSFXVolume();
-        //SetMusicVolume();
+        
     }
 
-    //void SetupResolutionDropdown()
-    //{
-    //    resolutions = Screen.resolutions;
-    //    resolutionDropdown.ClearOptions();
+    void SetupResolutionDropdown()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
 
-    //    List<string> options = new List<string>();
-    //    int currentResolutionIndex = 0;
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
 
-    //    for (int i = 0; i < resolutions.Length; i++)
-    //    {
-    //        string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRateRatio + "Hz";
-    //        options.Add(option);
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRateRatio + "Hz";
+            options.Add(option);
 
-    //        if (resolutions[i].width == Screen.currentResolution.width &&
-    //            resolutions[i].height == Screen.currentResolution.height)
-    //        {
-    //            currentResolutionIndex = i;
-    //        }
-    //    }
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
 
-    //    resolutionDropdown.AddOptions(options);
-    //    resolutionDropdown.value = currentResolutionIndex;
-    //    resolutionDropdown.RefreshShownValue();
-    //}
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetMusicVolume()
     {
